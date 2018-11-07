38a7215cc3757e721b5754f206cf3d83ebdbce5b
hanjjm <hanjjm1994@naver.com>
2018-11-08 02:40:40 +0900

    Development environment settings & Add 3 Tabs with ChangingTab adapter

---
+++ app/src/main/java/com/example/hanju/myapplication/ChangingTab.java
@@ -0,0 +1,37 @@
+package com.example.hanju.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class ChangingTab extends FragmentStatePagerAdapter {
+
+    int mNoOfTabs;
+
+    public ChangingTab(FragmentManager fm, int NumberofTabs){
+        super(fm);
+        this.mNoOfTabs = NumberofTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+            case 2:
+                Tab3 tab3 = new Tab3();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNoOfTabs;
+    }
+}
--- app/src/main/java/com/example/hanju/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hanju/myapplication/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.hanju.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import android.support.design.widget.TabLayout.TabLayoutOnPageChangeListener;
+import android.support.design.widget.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener, Tab2.OnFragmentInteractionListener, Tab3.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//Test
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 3"));
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final ChangingTab adapter = new ChangingTab(getSupportFragmentManager(), tabLayout.getTabCount());;
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+    }
+    public void makeTabs() {
+        //TabLayout tabLayout =  findViewById(R.id.tablayout);
+        //ChangingTab adapter = new ChangingTab(getSupportFragmentManager(), tabLayout.getTabCount());
+    }
+
 }
